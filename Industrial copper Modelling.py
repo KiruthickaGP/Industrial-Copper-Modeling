bab6024477d6e419911dd069366aec2e6fa5e67d
Kiruthicka GP <130207892+KiruthickaGP@users.noreply.github.com>
2023-11-25 11:42:56 +0530

    Data Preprocessing , EDA and machine learning

---
+++ Industrial copper Modelling.py
@@ -0,0 +1,468 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Industrial Copper Modeling
+
+# In[35]:
+
+
+#1 Importing libraries
+
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor,DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler, OneHotEncoder,LabelBinarizer
+from sklearn.metrics import mean_squared_error, r2_score,accuracy_score,confusion_matrix
+from sklearn.model_selection import GridSearchCV
+import pickle
+
+
+# In[2]:
+
+
+#2 Read the CSV file into a pandas dataframe
+
+df = pd.read_csv(r"E:\Guvidatascience\Projects\Industrial_Copper_Modeling\Copper_Set.csv")
+
+df.head(5)
+
+
+
+# In[3]:
+
+
+#3
+
+print(len(df['item_date'].unique())) #
+print(len(df['customer'].unique())) #
+print(len(df['material_ref'].unique()))
+print(len(df['product_ref'].unique())) #
+print(len(df['delivery date'].unique())) #
+
+
+# # Null values, shape and data types before dealing with incorrect data
+
+# In[4]:
+
+
+#4 
+df.shape
+
+
+# In[5]:
+
+
+#5 missing values
+missing_values_count = df.isnull().sum()
+print(missing_values_count)
+
+
+# In[6]:
+
+
+#6
+df.info()
+
+
+# # Dealing with data in wrong format
+
+# In[7]:
+
+
+#7
+# dealing with data in wrong format
+# for categorical variables, this step is ignored
+# df = df[df['status'].isin(['Won', 'Lost'])]
+df['item_date'] = pd.to_datetime(df['item_date'], format='%Y%m%d', errors='coerce').dt.date
+df['quantity tons'] = pd.to_numeric(df['quantity tons'], errors='coerce')
+df['customer'] = pd.to_numeric(df['customer'], errors='coerce')
+df['country'] = pd.to_numeric(df['country'], errors='coerce')
+df['application'] = pd.to_numeric(df['application'], errors='coerce')
+df['thickness'] = pd.to_numeric(df['thickness'], errors='coerce')
+df['width'] = pd.to_numeric(df['width'], errors='coerce')
+df['material_ref'] = df['material_ref'].str.lstrip('0')
+df['product_ref'] = pd.to_numeric(df['product_ref'], errors='coerce')
+df['delivery date'] = pd.to_datetime(df['delivery date'], format='%Y%m%d', errors='coerce').dt.date
+df['selling_price'] = pd.to_numeric(df['selling_price'], errors='coerce')
+
+
+# # Nan values and shape after dealing with data in wrong format
+
+# In[8]:
+
+
+#8
+missing_values_count = df.isnull().sum()
+print(missing_values_count)
+print(df.shape)
+df.info()
+
+
+# # Dealing with Missing Values
+
+# In[9]:
+
+
+#9 material_ref has large set of null values, so replacing them with unknown
+df['material_ref'].fillna('unknown', inplace=True)
+# deleting the remaining null values as they are less than 1% of data which can be neglected
+df = df.dropna()
+
+
+# In[10]:
+
+
+#10
+missing_values_count = df.isnull().sum()
+print(missing_values_count)
+print(df.shape)
+
+
+# In[11]:
+
+
+#11
+df_p=df.copy()
+
+
+# In[12]:
+
+
+df
+
+
+# In[13]:
+
+
+#12
+
+# ['quantity tons','selling_price','application','thickness','width', 'country']
+# ['status','item type']
+
+df_p['quantity tons']   # skewed
+df_p['country']
+df_p['application']
+df_p['thickness']       # skewed
+df_p['width']
+df_p['selling_price']   # skewed
+
+
+sns.distplot(df_p['quantity tons'])
+plt.show()
+sns.distplot(df_p['country'])
+plt.show()
+sns.distplot(df_p['application'])
+plt.show()
+sns.distplot(df_p['thickness'])
+plt.show()
+sns.distplot(df_p['width'])
+plt.show()
+sns.distplot(df_p['selling_price'])
+plt.show()
+
+
+# In[14]:
+
+
+#13
+mask1 = df_p['selling_price'] <= 0
+print(mask1.sum())
+df_p.loc[mask1, 'selling_price'] = np.nan
+
+mask1 = df_p['quantity tons'] <= 0
+print(mask1.sum())
+df_p.loc[mask1, 'quantity tons'] = np.nan
+
+mask1 = df_p['thickness'] <= 0
+print(mask1.sum())
+
+
+# In[15]:
+
+
+#14
+df_p.isnull().sum()
+
+
+# In[16]:
+
+
+#15
+df_p.dropna(inplace=True)
+len(df_p)
+     
+
+
+# In[17]:
+
+
+#16
+
+df_p['selling_price_log'] = np.log(df_p['selling_price'])
+sns.distplot(df_p['selling_price_log'])
+plt.show()
+
+df_p['quantity tons_log'] = np.log(df_p['quantity tons'])
+sns.distplot(df_p['quantity tons_log'])
+plt.show()
+
+df_p['thickness_log'] = np.log(df_p['thickness'])
+sns.distplot(df_p['thickness_log'])
+plt.show()
+
+
+# In[18]:
+
+
+#17
+
+df_p.head()
+
+
+# In[19]:
+
+
+#18
+
+x=df_p[['quantity tons_log','application','thickness_log','width','selling_price_log','country','customer','product_ref']].corr()
+sns.heatmap(x, annot=True, cmap="YlGnBu")
+
+
+# # DECISION TREE REGRESSOR
+# 
+
+# 
+# Encode categorical features
+# 
+# Test and Train split
+# 
+# Define Hyperparameters for GridSearchCV
+# 
+# Train the DecisionTreeRegressor with best parameters
+# 
+# Evalution metrics
+# 
+# Predict the selling price for new values
+# 
+# Save the DecisionTreeRegressor model
+
+# In[20]:
+
+
+#19
+
+X=df_p[['quantity tons_log','status','item type','application','thickness_log','width','country','customer','product_ref']]
+y=df_p['selling_price_log']
+
+# encoding categorical variables
+ohe = OneHotEncoder(handle_unknown='ignore')
+ohe.fit(X[['item type']])
+X_ohe = ohe.fit_transform(X[['item type']]).toarray()
+ohe2 = OneHotEncoder(handle_unknown='ignore')
+ohe2.fit(X[['status']])
+X_be = ohe2.fit_transform(X[['status']]).toarray()
+
+# independent features after encoding
+X = np.concatenate((X[['quantity tons_log', 'application', 'thickness_log', 'width','country','customer','product_ref']].values, X_ohe, X_be), axis=1)
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+# test and train split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+
+# decision tree
+dtr = DecisionTreeRegressor()
+
+# hyperparameters
+param_grid = {'max_depth': [2, 5, 10, 20],
+              'min_samples_split': [2, 5, 10],
+              'min_samples_leaf': [1, 2, 4],
+              'max_features': ['auto', 'sqrt', 'log2']}
+
+# gridsearchcv
+grid_search = GridSearchCV(estimator=dtr, param_grid=param_grid, cv=5)
+grid_search.fit(X_train, y_train)
+print("Best hyperparameters:", grid_search.best_params_)
+best_model = grid_search.best_estimator_
+y_pred = best_model.predict(X_test)
+
+# evalution metrics
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print('Mean squared error:', mse)
+print('R-squared:', r2)
+
+
+# In[21]:
+
+
+#20
+
+# ['quantity tons_log', 'application', 'thickness_log', 'width','country','customer','product_ref']].values, X_ohe, X_be
+new_sample = np.array([[np.log(40), 10, np.log(250), 0, 28,30202938,1670798778,'PL','Won']])
+new_sample_ohe = ohe.transform(new_sample[:, [7]]).toarray()
+new_sample_be = ohe2.transform(new_sample[:, [8]]).toarray()
+new_sample = np.concatenate((new_sample[:, [0,1,2, 3, 4, 5, 6,]], new_sample_ohe, new_sample_be), axis=1)
+new_sample1 = scaler.transform(new_sample)
+new_pred = best_model.predict(new_sample1)
+print('Predicted selling price:', np.exp(new_pred))
+
+
+# In[22]:
+
+
+#21
+# Saving the model
+
+with open('model.pkl', 'wb') as file:
+    pickle.dump(best_model, file)
+with open('scaler.pkl', 'wb') as f:
+    pickle.dump(scaler, f)
+with open('t.pkl', 'wb') as f:
+    pickle.dump(ohe, f)
+with open('s.pkl', 'wb') as f:
+    pickle.dump(ohe2, f)
+     
+
+
+# In[23]:
+
+
+#22
+
+print(len(df_p))
+df_p.head(3)
+
+
+# In[24]:
+
+
+#23
+
+df_c = df_p[df_p['status'].isin(['Won', 'Lost'])]
+len(df_c)
+
+
+# # DECISION TREE CLASSIFIER
+
+# In[36]:
+
+
+#24
+Y = df_c['status']
+X= df_c[['quantity tons_log','selling_price_log','item type','application','thickness_log','width','country','customer','product_ref']]
+
+# encoding categorical variables
+ohe = OneHotEncoder(handle_unknown='ignore')
+ohe.fit(X[['item type']])
+X_ohe = ohe.fit_transform(X[['item type']]).toarray()
+be = LabelBinarizer()
+be.fit(Y)
+y = be.fit_transform(Y)
+# independent features after encoding
+X = np.concatenate((X[['quantity tons_log', 'selling_price_log','application', 'thickness_log', 'width','country','customer','product_ref']].values, X_ohe), axis=1)
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# decision tree classifier
+dtc = DecisionTreeClassifier()
+dtc.fit(X_train, y_train)
+y_pred = dtc.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
+cm = confusion_matrix(y_test, y_pred)
+print(f"Confusion Matrix:\n{cm}")
+
+expalin the code in 5 lines for presentation - simple terms
+
+
+# In[39]:
+
+
+#25
+
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+print("Classification Report:")
+print(classification_report(y_test, y_pred))
+# ROC curve and AUC
+fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+roc_auc = auc(fpr, tpr)
+plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.show()
+
+
+# In[40]:
+
+
+#26
+with open('clsmodel.pkl', 'wb') as file:
+    pickle.dump(dtc, file)
+with open('cscaler.pkl', 'wb') as f:
+    pickle.dump(scaler, f)
+with open('ct.pkl', 'wb') as f:
+    pickle.dump(ohe, f)
+     
+
+
+# In[38]:
+
+
+#27
+
+# Predict the status for a new sample
+# 'quantity tons_log', 'selling_price_log','application', 'thickness_log', 'width','country','customer','product_ref']].values, X_ohe
+new_sample = np.array([[np.log(700), np.log(956), 10, np.log(2),1500,28.0,30202938,1670798778,'W']])
+new_sample_ohe = ohe.transform(new_sample[:, [8]]).toarray()
+new_sample = np.concatenate((new_sample[:, [0,1,2, 3, 4, 5, 6,7]], new_sample_ohe), axis=1)
+new_sample = scaler.transform(new_sample)
+new_pred = dtc.predict(new_sample)
+if new_pred==1:
+    print('The status is: Won')
+else:
+    print('The status is: Lost')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
